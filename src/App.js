4b12ab81d407e070f5938f5238577de2ba88362d
Malcom Johnson <malcom7johnson@gmail.com.com>
2017-03-25 21:10:24 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import Stopwatch from "./Stopwatch";
+
+class App extends Component {
+  render() {
+    return (
+      <Stopwatch />
+    );
+  }
+}
+
+export default App;
